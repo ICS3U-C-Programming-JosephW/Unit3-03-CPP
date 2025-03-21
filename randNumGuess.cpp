4d5e96a176954fbc99c5f7e6446f3b8298bad82e
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-03-21 21:20:11 +0000

    Added randNumGuess.cpp

---
+++ randNumGuess.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2025. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: Mar. 21, 2025
+// This program creates a guessing game with a number range 0-9.
+
+// Includes the iostream library for input and output functions.
+#include <iostream>
+// Include the random library for random number generators.
+#include <random>
+
+int main() {
+    // Declare some random number as an integer.
+    int correctNumber;
+    // Creates the seed.
+    std::random_device rseed;
+    // Generates the seed using the mersenne twister.
+    std::mt19937 rgen(rseed());
+    // Distributes the possible integers over a distance of 0-9.
+    std::uniform_int_distribution<int> idist(0, 9);  
+    // Sets the random number to the distributed integer.
+    correctNumber = idist(rgen);
+
+    // Declare the variable for the chosen number.
+    int chosenNumber;
+
+    // Asks the user to guess a number between 0 and 9.
+    std::cout << "Guess a number between 0 and 9: ";
+    // Stores the user's choice in the chosen number variable.
+    std::cin >> chosenNumber;
+
+    // Check if the user chose the correct number.
+    if (chosenNumber == correctNumber) {
+        // Displays they got it correct.
+        std::cout << "You guessed correct!" << std::endl;
+    }
+    // Otherwise, check if the user did not choose the correct number.
+    else {
+        // Displays they got it wrong, and they must try again.
+        std::cout << "You guessed wrong! The correct answer was "
+        << correctNumber << "." << std::endl;
+    }
+}
